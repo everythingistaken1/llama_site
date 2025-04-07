2cb4b6ca2b9e6beae17c45cd42ce64baa0ff7b02
everythingistaken1 <154841641+everythingistaken1@users.noreply.github.com>
2025-04-07 14:28:18 +0000

    Create config.js

---
+++ config.js
@@ -0,0 +1,37 @@
+var klaroConfig = {
+    version: 1,
+    elementID: 'klaro',
+    noAutoLoad: false,
+    htmlTexts: true,
+    embedded: false,
+    storageMethod: 'cookie',
+    cookieName: 'klaro',
+    cookieExpiresAfterDays: 120,
+    default: false,
+    mustConsent: true,
+    acceptAll: true,
+    hideDeclineAll: false,
+    services: [
+      {
+        name: 'google-analytics',
+        default: false,
+        purposes: ['analytics'],
+        cookies: [/^_ga/, /^_gid/],
+        required: false,
+        optOut: false,
+        onlyOnce: false
+      }
+    ],
+    translations: {
+      en: {
+        privacyPolicyUrl: '/privacy',
+        consentModal: {
+          title: 'Privacy settings',
+          description: 'Here you can see and customize the information that we collect about you.'
+        },
+        'google-analytics': {
+          description: 'Google Analytics is used to collect site usage statistics.'
+        }
+      }
+    }
+};
